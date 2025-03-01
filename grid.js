e998ad784fb64fe4f4d74d9910231a711be97530
Rudraksh <rudrakshkumar2908@gmail.com>
2025-03-01 18:40:06 +0530

    Done with the project! Added header with text Etch A Sketch Added 5 buttons :- Adjust Grid, Reset, Black, Rainbow, Darker Added grid in middle Added footer of my github repo

---
+++ grid.js
@@ -0,0 +1,88 @@
+const container = document.querySelector(".container");
+const adjust_grid = document.querySelector(".adjust-grid");
+const black = document.querySelector(".black");
+const rainbow = document.querySelector(".rainbow");
+const darker = document.querySelector(".darker");
+const reset = document.querySelector(".reset");
+const github = document.querySelector(".github");
+let column
+
+adjust_grid.addEventListener("click", () => {
+    container.innerHTML = ""
+    while(true){
+        input = prompt("Enter number of grids :- ");
+
+        if(input == "" || input == null){
+            alert("User action cancelled!");
+            break
+        }
+        else if(input > 100){
+            alert("Please enter input less than 100!");
+            continue
+        }
+        else{
+            makeGrid(input);
+            break
+        }
+    }
+});
+
+function makeGrid(num = 16){
+    for(let i = 0; i < num; i++){
+        const column_div = document.createElement("colum-div");
+        column_div.classList.add(`column${i}`);
+        column_div.classList.add("column")
+
+        for(let rows = 0; rows < num; rows++){
+            const row_div = document.createElement("row-div");
+            row_div.classList.add(`row${i}`);
+            row_div.classList.add("row");
+            column_div.appendChild(row_div);
+
+            row_div.addEventListener("mouseover", () =>{
+                row_div.style.cssText = "background-color: rgba(37,37,40,0.85)";
+            })
+
+            black.addEventListener("click", () =>{
+                row_div.addEventListener("mouseover", () =>{
+                    row_div.style.cssText = "background-color: rgba(37,37,40,0.85)";
+                })
+            })
+
+            rainbow.addEventListener("click", function newColor(){
+
+                row_div.addEventListener("mouseover", () =>{
+                    let red = Math.random() * 256;
+                    let green = Math.random() * 256;
+                    let blue = Math.random() * 256;
+
+                    rgbColor = `rgb(` + red + ',' + green + ',' + blue + `)`;
+                    
+                    row_div.style.backgroundColor = rgbColor;
+                })
+            });
+
+            reset.addEventListener("click", () => {
+                row_div.style.cssText = "background-color: transparent";
+            })
+            let alpha = 0.1;
+
+            darker.addEventListener("click", () => {
+                row_div.addEventListener("mouseover", () =>{ 
+                    alpha += 0.1;
+                    row_div.style.backgroundColor = `rgba(37,37,40, ${alpha})`;
+                });
+            })
+            
+        }
+        
+        container.appendChild(column_div);
+    }
+}
+
+makeGrid();
+
+
+github.addEventListener("click", () =>{
+    window.open('https://github.com/newbbiecoder', '_blank');
+})
